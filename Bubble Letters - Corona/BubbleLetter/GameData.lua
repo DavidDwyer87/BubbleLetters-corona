4b60c1a8984c219f466a1c5b409a303a5c309757
DavidDwyer87 <David_Dwyer@live.com>
2014-12-02 13:01:41 -0500

    Minor change v 1.3.2

--- Bubble Letters - Corona/BubbleLetter/GameData.lua
+++ Bubble Letters - Corona/BubbleLetter/GameData.lua
@@ -7,14 +7,15 @@ local current = 0
 local allballs = {}
 local startTimer = true
 
+local path = system.pathForFile("settings.txt", system.DocumentsDirectory)
+
 
 
 function setTopWordScore(score,tword)
-
-	if (score>wordScore) then
+	if (tonumber(score)>tonumber(wordScore)) then
 		wordScore = score
 		word = tword
-	end	
+	end
 	write_Settings()
 end
 
@@ -25,6 +26,7 @@ function setScore(score)
 	if (scoreNum==nil) then
 		scoreNum=0
 	end
+	print(scoreNum)
 
 	if (tonumber(score)>tonumber(scoreNum)) then
 		scoreNum = score
@@ -67,30 +69,30 @@ end
 
 function Read_Settings()
   
-  local path = system.pathForFile( "settings.txt", system.DocumentsDirectory )
+  
   local saveData = "topWordScore=\n"..
   					"topword=\n"..
                    "topScore=\n"..
                    "current=\n"..
                    "sound=true\n"
 
-  if(path ~= nil)then
-    local file = io.open( path, "w" )
-    file:write( saveData )
 
-	io.close( file )
-	file = nil
-  else
-	local file = io.open( path, "r" )
-	for line in file:lines() do
+
+  -- io.open opens a file at path. returns nil if no file found
+  local fileHandle, errorString = io.open( path, "r" )
+
+  if fileHandle then  	
+  	
+	for line in fileHandle:lines() do
 
 		--get top word score
 		if(string.find(line,"topWordScore="))then
-			if(line:sub(14)==nil)then
+			if(line:sub(14)=="")then
 				wordScore = 0
 			else
-				wordScore = line:sub(14)
-			end			 
+				wordScore = line:sub(14)				
+			end	
+			print("hello it works"..wordScore)		 
 		end
 
 		if(string.find(line,"topword="))then
@@ -110,7 +112,7 @@ function Read_Settings()
 		end
 
 		if(string.find(line,"topScore="))then
-			if(line:sub(10)==nil)then
+			if(line:sub(10)=="")then
 				scoreNum=0
 			else
 				scoreNum=line:sub(10)
@@ -125,7 +127,15 @@ function Read_Settings()
     	end
 	end
 
+	io.close( fileHandle )
+	fileHandle = nil
+  else
+  	 local file = io.open( path, "w" )
+    file:write( saveData )
+
+	io.flush()
 	io.close( file )
+
 	file = nil
   end  
 end
@@ -138,17 +148,16 @@ function write_Settings()
 		state = "false"
 	end
 
-	local path = system.pathForFile( "settings.txt", system.DocumentsDirectory )
-
 	local saveData = "topWordScore="..wordScore.."\n"..
 					"topword="..word.."\n"..
                    "topScore="..scoreNum.."\n"..
                    "current="..current.."\n"..
                    "sound="..state
-
+    --print(saveData)
     local file = io.open( path, "w" )
     file:write( saveData )
 
+    io.flush()
 	io.close( file )
 	file = nil
 end
